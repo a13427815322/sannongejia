e35bbf89b8cb1e4a80e9ade3d81a4b7eb3829775
xgz <xgz@sise.com>
2023-05-14 18:51:38 +0800

    首页的样式和渲染

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,26 +2,26 @@
 	<view class="search">
 <image src="../../static/saerch.svg" ></image>
 				<input placeholder="请输入要查找的内容"/>
-	</view>
+</view>
 	<view >
 		<swiper class="newswiper" indicator-dots="true" indicator-color="#FFFFFF" indicator-active-color="#24AC6B" autoplay="true" interval="3000" circular="true" >
 			<swiper-item v-for="(item,index) in banner" ><image :src="item.url" mode="scaleToFill" ></image></swiper-item>
 		</swiper>
 	</view>
 	<view class="newsbox">
-	相关资讯
+	<view class="xgzx">相关资讯</view>
 	<scroll-view scroll-x class="tabs-title" style="white-space: nowrap"   >
 		<view  v-for="(item,index) in tabList" :key="index" @click="ontabtap(index)">
 			<span :class="active===index?'active':''">	{{item.name}}</span>
 		</view>		</scroll-view>
 		<view v-for="(item,index) in news" >
-			<view>
+			<view class="itembox">
 				<view class="newleft">
-					<view>{{item.title}}</view>
-					<view><view>{{item.soure}}</view><view>{{item.datetime}}</view></view>
+					<view class="newtitle">{{item.title}}</view>
+					<view><view class="newsoure">{{item.soure}}</view><view class="newdatetime">{{item.datetime}}</view></view>
 				</view>
-				<view class="newimg"><image :src="item.img_url" mode="aspectFill"></image></view>
-				
+				<view class="newimg"><image :src="item.img_url" mode="aspectFit"></image></view>
+				<view style="clear: both;"></view>
 			</view>
 			<view style="clear: both;"></view>
 		</view>
@@ -70,7 +70,10 @@
 						console.log(res)
 						for(let index in res.data){
 							res.data[index].datetime=new Date(res.data[index].datetime).toLocaleDateString();
-							
+							console.log(res.data[index].title.length)
+							if(res.data[index].title.length>=30){
+								res.data[index].title=res.data[index].title.substring(0,30)+"..."
+							}
 						}
 						// res.data.datetime.substring(0,5)
 						this.news = res.data
